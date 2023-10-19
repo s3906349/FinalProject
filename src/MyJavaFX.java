10cbc319f61ea668092146ce8c7ad1e1dc5ddd84
s3906349 <s3906349@student.rmit.edu.au>
2023-10-19 17:34:10 +1100

    testing

--- src/MyJavaFX.java
+++ src/MyJavaFX.java
@@ -13,10 +13,7 @@ public class MyJavaFX extends Application {
     primaryStage.setScene(scene); // Place the scene in the stage
     primaryStage.show(); // Display the stage
   }
-    /**
-   * The main method is only needed for the IDE with limited
-   * JavaFX support. Not needed for running from the command line.
-   */
+    
   public static void main(String[] args) { 
     launch(args);
   }
